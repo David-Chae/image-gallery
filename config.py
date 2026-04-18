2adc64dba34b5d37c34622184184d1f0dda01360
David-Chae <aksrud859@hanmail.net>
2026-04-19 00:51:46 +1000

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 PROJECT_ROOT = Path(__file__).resolve().parent
 
 # ---- Core paths ----
-ROOT_DIR = Path(os.environ.get("IMAGE_GALLERY_ROOT", r"E:\miscellaneous\hitomi"))
+ROOT_DIR = Path(os.environ.get("IMAGE_GALLERY_ROOT", r"E:\")) #Please replace "E:\" with your root directory containing folders of images.
 DB_PATH = PROJECT_ROOT / "gallery.db"
 THUMB_CACHE_DIR = PROJECT_ROOT / "thumb_cache"
 STATIC_DIR = PROJECT_ROOT / "static"
